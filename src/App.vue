f04dd29963aba70ff9c5a17eb4ed9a4071b3f02b
Evgen904 <evgen904@gmail.com>
2019-02-05 00:28:42 +0300

    vue-animation

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container text-center pt-5">
+    <button class="btn btn-primary mb-5" @click="show = !show">Toggle</button>
+
+    <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, saepe?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
