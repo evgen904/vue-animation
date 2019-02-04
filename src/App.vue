8ed7c5812ebe42407f1f7412a4795a88608b8c44
Evgen904 <evgen904@gmail.com>
2019-02-05 00:38:10 +0300

    Этапы анимации

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div class="container text-center pt-5">
     <button class="btn btn-primary mb-5" @click="show = !show">Toggle</button>
 
-    <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, saepe?</p>
+    <transition name="ma">
+      <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, saepe?</p>
+    </transition>
   </div>
 </template>
 
@@ -17,5 +19,13 @@ export default {
 </script>
 
 <style>
+
+  .ma-enter {}
+  .ma-enter-active {}
+  .ma-enter-to {}
+
+  .ma-leave {}
+  .ma-leave-active {}
+  .ma-leave-to {}
 
 </style>
