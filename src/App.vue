3a54accca26bc7017af01edc784c1d27ab1d7d93
Evgen904 <evgen904@gmail.com>
2019-02-05 00:54:54 +0300

    Создание анимации

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div class="container text-center pt-5">
     <button class="btn btn-primary mb-5" @click="show = !show">Toggle</button>
 
-    <transition name="ma">
+    <transition
+      name="ma"
+      type="animation"
+    >
       <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, saepe?</p>
     </transition>
   </div>
@@ -20,12 +23,30 @@ export default {
 
 <style>
 
-  .ma-enter {}
-  .ma-enter-active {}
+  .ma-enter {
+    opacity: 0;
+  }
+  .ma-enter-active {
+    transition: opacity 1s;
+  }
   .ma-enter-to {}
 
   .ma-leave {}
-  .ma-leave-active {}
-  .ma-leave-to {}
+  .ma-leave-active {
+    animation: 1s ma-slide forwards;
+    transition: opacity 3s;
+  }
+  .ma-leave-to {
+    opacity: 0;
+  }
+
+  @keyframes ma-slide {
+    from {
+      transform: translateX(0px);
+    }
+    to {
+      transform: translateX(-100px);
+    }
+  }
 
 </style>
